ff86829ec7ffc975dbf5e390a2efd68b23d502ba
Joseph Yao <joseph.yao.ruozhou@gmail.com>
2019-05-12 20:35:51 +0200

    :sparkles: implement exercise 1 by tdd

--- src/exercise/exercise_1.c
+++ src/exercise/exercise_1.c
@@ -1,9 +1,18 @@
 #include "tile.h"
+#include <string.h>
+#include <stdbool.h>
+
+bool containNeighbors(const tile_t *tiles, int tile_count, int firstNeighbor, int secondNeighbor);
 
 int tiles_is_able_to_chow(tile_t tiles[], int tile_count, tile_t tile)
 {
-	/*
-	 * exercise: add your code here.
-	 */
+    if (containNeighbors(tiles, tile_count, tile + 1, tile + 2) || containNeighbors(tiles, tile_count, tile - 1, tile + 1) ||
+    containNeighbors(tiles, tile_count, tile - 1, tile - 2)) {
+        return 1;
+    }
 	return 0;
+}
+
+bool containNeighbors(const tile_t *tiles, int tile_count, int firstNeighbor, int secondNeighbor) {
+    return memchr(tiles, firstNeighbor, tile_count) && memchr(tiles, secondNeighbor, tile_count);
 }
--- tst/test_exercise_1.cpp
+++ tst/test_exercise_1.cpp
@@ -2,16 +2,50 @@
 #include <string.h>
 #include <stdlib.h>
 #include <stdio.h>
+
 extern "C" {
 #include "tiles.h"
 }
 
 TEST_GROUP(tiles_is_able_to_chow) {
+    void setup() {
+
+    }
+
+    void teardown() {
+
+    }
 
 };
 
-IGNORE_TEST(tiles_is_able_to_chow, start_here)
-{
-	FAIL("START HERE");
+TEST(tiles_is_able_to_chow, notAbleToChow) {
+    tile_t tiles[] = {MJ_EAST, MJ_SOUTH, MJ_WEST, MJ_NORTH};
+
+    int result = tiles_is_able_to_chow(tiles, sizeof(tiles), MJ_ZHONG);
+
+    CHECK_TRUE(!result)
 }
 
+TEST(tiles_is_able_to_chow, chowWhenTileOnTheLeft) {
+    tile_t tiles[] = {MJ_CHARACTOR(2), MJ_CHARACTOR(3), MJ_EAST, MJ_EAST};
+
+    int result = tiles_is_able_to_chow(tiles, sizeof(tiles), MJ_CHARACTOR(1));
+
+    CHECK_TRUE(result)
+}
+
+TEST(tiles_is_able_to_chow, chowWhenTileInTheMiddle) {
+    tile_t tiles[] = {MJ_CHARACTOR(1), MJ_CHARACTOR(3), MJ_EAST, MJ_EAST};
+
+    int result = tiles_is_able_to_chow(tiles, sizeof(tiles), MJ_CHARACTOR(2));
+
+    CHECK_TRUE(result)
+}
+
+TEST(tiles_is_able_to_chow, chowWhenTileOnTheRight) {
+    tile_t tiles[] = {MJ_CHARACTOR(1), MJ_CHARACTOR(2), MJ_EAST, MJ_EAST};
+
+    int result = tiles_is_able_to_chow(tiles, sizeof(tiles), MJ_CHARACTOR(3));
+
+    CHECK_TRUE(result)
+}
