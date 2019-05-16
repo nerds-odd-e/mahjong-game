850dab764cc40f43deffa4d6a07a680c91e20268
Joseph Yao <joseph.yao.ruozhou@gmail.com>
2019-05-16 11:17:42 +0200

    :recycle: exercise 6# with fff

--- tst/test_exercise_2.cpp
+++ tst/test_exercise_2.cpp
@@ -1,69 +1,64 @@
 #include "CppUTest/TestHarness.h"
-#include "CppUTestExt/MockSupport.h"
 #include <string.h>
 #include <stdlib.h>
 #include <stdio.h>
 
 extern "C" {
 #include "exercise_3.h"
+#include "fff.h"
 }
+
+DEFINE_FFF_GLOBALS
+FAKE_VALUE_FUNC1(int, user_data_find, int)
+FAKE_VALUE_FUNC1(int, user_data_get_score, int)
+FAKE_VOID_FUNC1(user_data_add, int)
+FAKE_VOID_FUNC2(user_data_set_score, int, int)
 
 TEST_GROUP(player_wins) {
 
     int player_id;
     void setup() {
         player_id = 10;
-        mock().ignoreOtherCalls();
+        RESET_FAKE(user_data_find)
     }
 
     void given_user_data_not_found() {
-        mock().expectOneCall("user_data_find").withParameter("player_id", player_id).andReturnValue(0);
+        user_data_find_fake.return_val = 0;
     }
 
     void given_user_data_found() {
-        mock().expectOneCall("user_data_find").withParameter("player_id", player_id).andReturnValue(1);
+        user_data_find_fake.return_val = 1;
     }
 
     void given_user_data_get_score(int score) {
-        mock().expectOneCall("user_data_get_score").withParameter("player_id", player_id).andReturnValue(score);
+        user_data_get_score_fake.return_val = score;
     }
 
 };
 
 TEST(player_wins, addUserIfNotFound) {
     given_user_data_not_found();
-    mock().expectOneCall("user_data_add").withParameter("player_id", player_id);
 
     player_wins(player_id);
+
+    CHECK_EQUAL(1, user_data_add_fake.call_count)
 }
 
 TEST(player_wins, notAddUserIfFound) {
     given_user_data_found();
-    mock().expectNoCall("user_data_add");
 
     player_wins(player_id);
+
+    CHECK_EQUAL(0, user_data_add_fake.call_count)
 }
 
 TEST(player_wins, plusOneToUserScore) {
     given_user_data_found();
     given_user_data_get_score(5);
-    mock().expectOneCall("user_data_set_score").withParameter("player_id", player_id).withParameter("score", 6);
 
     player_wins(player_id);
+
+    CHECK_EQUAL(1, user_data_set_score_fake.call_count)
+    CHECK_EQUAL(6, user_data_set_score_fake.arg1_val)
 }
 
-int user_data_find(int player_id) {
-    return mock().actualCall("user_data_find").withParameter("player_id", player_id).returnValue().getIntValue();
-}
-
-void user_data_add(int player_id) {
-    mock().actualCall("user_data_add").withParameter("player_id", player_id);
-}
-
-int user_data_get_score(int player_id) {
-    return mock().actualCall("user_data_get_score").withParameter("player_id", player_id).returnValue().getIntValue();
-}
-
-void user_data_set_score(int player_id, int score) {
-    mock().actualCall("user_data_set_score").withParameter("player_id", player_id).withParameter("score", score);
-}
