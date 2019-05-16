e6ee3e42e436c36606694c32d642298768faf3de
Joseph Yao <joseph.yao.ruozhou@gmail.com>
2019-05-16 11:07:27 +0200

    :recycle: exercise 6# with cppumock

--- tst/test_exercise_2.cpp
+++ tst/test_exercise_2.cpp
@@ -6,38 +6,64 @@
 
 extern "C" {
 #include "exercise_3.h"
-#include "stub_user_data.h"
 }
 
 TEST_GROUP(player_wins) {
 
+    int player_id;
     void setup() {
-        reset();
+        player_id = 10;
+        mock().ignoreOtherCalls();
+    }
+
+    void given_user_data_not_found() {
+        mock().expectOneCall("user_data_find").withParameter("player_id", player_id).andReturnValue(0);
+    }
+
+    void given_user_data_found() {
+        mock().expectOneCall("user_data_find").withParameter("player_id", player_id).andReturnValue(1);
+    }
+
+    void given_user_data_get_score(int score) {
+        mock().expectOneCall("user_data_get_score").withParameter("player_id", player_id).andReturnValue(score);
     }
 
 };
 
 TEST(player_wins, addUserIfNotFound) {
-    given_user_data_find(0);
+    given_user_data_not_found();
+    mock().expectOneCall("user_data_add").withParameter("player_id", player_id);
 
-    player_wins(0);
-
-    CHECK_TRUE(addCalled)
+    player_wins(player_id);
 }
 
 TEST(player_wins, notAddUserIfFound) {
-    given_user_data_find(1);
+    given_user_data_found();
+    mock().expectNoCall("user_data_add");
 
-    player_wins(0);
-
-    CHECK_FALSE(addCalled)
+    player_wins(player_id);
 }
 
 TEST(player_wins, plusOneToUserScore) {
-    given_user_data_find(1);
-    given_user_score(5);
+    given_user_data_found();
+    given_user_data_get_score(5);
+    mock().expectOneCall("user_data_set_score").withParameter("player_id", player_id).withParameter("score", 6);
 
-    player_wins(0);
+    player_wins(player_id);
+}
 
-    CHECK_EQUAL(6, user_score)
+int user_data_find(int player_id) {
+    return mock().actualCall("user_data_find").withParameter("player_id", player_id).returnValue().getIntValue();
+}
+
+void user_data_add(int player_id) {
+    mock().actualCall("user_data_add").withParameter("player_id", player_id);
+}
+
+int user_data_get_score(int player_id) {
+    return mock().actualCall("user_data_get_score").withParameter("player_id", player_id).returnValue().getIntValue();
+}
+
+void user_data_set_score(int player_id, int score) {
+    mock().actualCall("user_data_set_score").withParameter("player_id", player_id).withParameter("score", score);
 }
